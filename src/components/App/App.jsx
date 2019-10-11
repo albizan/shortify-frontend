60ebe6c20fc045f3457c4e2a9bfdaf298a7068c3
Alberto Zanotti <albizan@hotmail.it>
2019-10-11 23:32:54 +0200

    Add LoginPage to Router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import RegisterPage from '../../pages/RegisterPage'
+import LoginPage from '../../pages/LoginPage'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Switch>
           <Route exact path='/' render={props => <p>Hello</p>} />
           <Route exact path='/register' component={RegisterPage} />
+          <Route exact path='/login' component={LoginPage} />
         </Switch>
       </BrowserRouter>
     </Fragment>
