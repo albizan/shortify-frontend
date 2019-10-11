c6feff3329c6ad91ce35a02d529b0cfd8fdc7449
Alberto Zanotti <albizan@hotmail.it>
2019-10-11 23:31:54 +0200

    Add Login functionality

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginForm from '../../components/LoginForm'
+
+const RegisterPage = props => {
+  return (
+    <div className='flex justify-center items-center h-screen w-full'>
+      <LoginForm />
+    </div>
+  )
+}
+
+export default RegisterPage
