e6895ce07aa34ce497288402bb199211f9925e4e
Alberto Zanotti <albizan@hotmail.it>
2019-10-11 23:32:36 +0200

    Add loginUser function

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,15 +1,12 @@
 import http from '../apis'
 
-export async function registerUser(
-  values,
-  { props, setSubmitting, setErrors, resetForm }
-) {
+export async function registerUser(values, { props, setSubmitting, setErrors, resetForm }) {
   setSubmitting(true)
   try {
     const { data } = await http.post('/auth/register', {
       name: values.name,
       email: values.email,
-      password: values.password,
+      password: values.password
     })
     const user = JSON.stringify(data.user)
     const jwt = JSON.stringify(data.jwt)
@@ -19,12 +16,37 @@ export async function registerUser(
     // Set user in redux store
     props.setLogin(user)
   } catch (error) {
+    props.setLogout()
     const { status } = error.response
     if (status === 409) {
       setErrors({
-        email: 'Email already used',
+        email: 'Email already used'
       })
     }
+  } finally {
+    setSubmitting(false)
+  }
+}
+
+export async function loginUser(values, { props, setSubmitting, setErrors, resetForm }) {
+  setSubmitting(true)
+  try {
+    const { data } = await http.post('/auth/login', {
+      email: values.email,
+      password: values.password
+    })
+    const user = JSON.stringify(data.user)
+    const jwt = JSON.stringify(data.jwt)
+    localStorage.setItem('user', user)
+    localStorage.setItem('jwt', jwt)
+    resetForm()
+    // Set user in redux store
+    props.setLogin(user)
+  } catch (error) {
+    props.setLogout()
+    setErrors({
+      email: 'Invalid Credentials'
+    })
   } finally {
     setSubmitting(false)
   }
