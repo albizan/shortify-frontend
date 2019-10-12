07e7463974bd6b3028db722753115b02d2329715
Alberto Zanotti <albizan@hotmail.it>
2019-10-12 12:28:00 +0200

    export new form reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 
 import { authReducer } from './auth.reducer'
+import { formReducer } from './form.reducer'
 
 const reducers = combineReducers({
   authState: authReducer,
+  formState: formReducer
 })
 
 export default reducers
