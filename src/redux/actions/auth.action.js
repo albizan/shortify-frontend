bf7799eef0e87541dbfd97e5d4799a9d3f5bb6ed
Alberto Zanotti <albizan@hotmail.it>
2019-10-12 12:26:52 +0200

    Bugfix, payload is a user object now

--- src/redux/actions/auth.action.js
+++ src/redux/actions/auth.action.js
@@ -1,15 +1,15 @@
 import { USER_LOGS_IN, USER_LOGS_OUT } from '../types'
 
-export function setLogin(user) {
+export function onLogin(user) {
   return {
     type: USER_LOGS_IN,
-    payload: user,
+    payload: user
   }
 }
 
-export function setLogout() {
+export function onLogout() {
   return {
     type: USER_LOGS_OUT,
-    payload: null,
+    payload: null
   }
 }
