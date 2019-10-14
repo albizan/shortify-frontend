513d4d39e659b4bb91d3051c9caaf0c2ed14f683
Alberto <albizan@hotmail.it>
2019-10-14 16:45:27 +0200

    Show Landing as root page of the web app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,15 +3,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import RegisterPage from '../../pages/RegisterPage'
 import LoginPage from '../../pages/LoginPage'
+import Landing from '../../pages/Landing/Landing'
 
 const App = () => {
   return (
     <Fragment>
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' render={props => <p>Hello</p>} />
-          <Route exact path='/register' component={RegisterPage} />
-          <Route exact path='/login' component={LoginPage} />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={RegisterPage} />
+          <Route exact path="/login" component={LoginPage} />
         </Switch>
       </BrowserRouter>
     </Fragment>
