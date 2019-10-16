7fdee1a06acb546fb94112e65b9eaac02a34f49b
Alberto <albizan@hotmail.it>
2019-10-16 11:55:07 +0200

    Configure redux store to be persisted

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,8 +1,21 @@
 import { createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import reducers from '../reducers'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
 export const store = createStore(
-  reducers,
+  persistedReducer,
   // Used to activate redux dev tools extension for google chrome
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+export const persistor = persistStore(store)
+
+/* persistor is used by redux-persistor to persist redux store in localstorage */
