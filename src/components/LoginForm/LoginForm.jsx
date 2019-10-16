66325a8cef02bd99dd320395cee058942d173cdc
Alberto <albizan@hotmail.it>
2019-10-16 13:48:34 +0200

    Clear form state before mounting

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,42 +1,58 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Field, withFormik } from 'formik'
 import validationSchema from './validationSchema'
 
 import FormError from '../FormError'
 import { loginUser } from '../../services'
-import { onLogin, onLogout, onLoginCompleted, onLoginFailed } from '../../redux/actions'
+import {
+  onLogin,
+  onLogout,
+  onLoginCompleted,
+  onLoginFailed,
+  cleanFormState,
+} from '../../redux/actions'
 
-const RegisterForm = ({ handleSubmit, errors, touched, formState }) => {
+const RegisterForm = ({
+  handleSubmit,
+  errors,
+  touched,
+  formState,
+  cleanFormState,
+}) => {
+  useEffect(() => {
+    cleanFormState()
+    // eslint-disable-next-line
+  }, [])
   return (
-    <div className='px-20 py-12 w-136 rounded-lg shadow-lg border border-gray-300'>
-      <h2 className='text-4xl mb-12 font-thin text-center'>Log In</h2>
-      <form className='text-xl' onSubmit={handleSubmit}>
+    <div className="px-20 py-12 w-136 rounded-lg shadow-lg border border-gray-300">
+      <h2 className="text-4xl mb-12 font-thin text-center">Log In</h2>
+      <form className="text-xl" onSubmit={handleSubmit}>
         <Field
-          className='block rounded-full py-3 px-5 bg-white border border-gray-400 mx-auto w-full'
-          type='email'
-          name='email'
-          id='email'
-          placeholder='e-mail'
+          className="block rounded-full py-3 px-5 bg-white border border-gray-400 mx-auto w-full"
+          type="email"
+          name="email"
+          id="email"
+          placeholder="e-mail"
         />
         <FormError message={errors.email} touched={touched.email} />
 
         <Field
-          className='block rounded-full py-3 px-5 bg-white border border-gray-400 mx-auto w-full'
-          type='password'
-          name='password'
-          id='password'
-          placeholder='Password'
+          className="block rounded-full py-3 px-5 bg-white border border-gray-400 mx-auto w-full"
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
         />
         <FormError message={errors.password} touched={touched.password} />
 
         <button
-          className='block rounded-full mt-10 py-3 px-5 bg-indigo-600 border border-indigo-600 mx-auto w-full text-white'
-          type='submit'
+          className="block rounded-full mt-10 py-3 px-5 bg-indigo-600 border border-indigo-600 mx-auto w-full text-white"
+          type="submit"
         >
           Log In
         </button>
-        {<p className='text-lg font-semibold'>{formState.message}</p>}
+        {<p className="text-lg font-semibold">{formState.message}</p>}
       </form>
     </div>
   )
@@ -46,17 +62,17 @@ const EnhancedRegisterForm = withFormik({
   mapPropsToValues: ({ email, password }) => {
     return {
       email: email || '',
-      password: password || ''
+      password: password || '',
     }
   },
   validationSchema: validationSchema,
-  handleSubmit: loginUser
+  handleSubmit: loginUser,
 })(RegisterForm)
 
 function mapStatetoProps(store) {
   return {
     authState: store.authState,
-    formState: store.formState
+    formState: store.formState,
   }
 }
 
@@ -66,6 +82,7 @@ export default connect(
     onLogin,
     onLogout,
     onLoginCompleted,
-    onLoginFailed
+    onLoginFailed,
+    cleanFormState,
   }
 )(EnhancedRegisterForm)
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Field, withFormik } from 'formik'
 import validationSchema from './validationSchema'
@@ -9,48 +9,62 @@ import {
   onLogin,
   onLogout,
   onRegistrationCompleted,
-  onRegistrationFailed
+  onRegistrationFailed,
+  cleanFormState,
 } from '../../redux/actions'
 
-const RegisterForm = ({ handleSubmit, errors, touched, authState, formState }) => {
+const RegisterForm = ({
+  handleSubmit,
+  errors,
+  touched,
+  formState,
+  cleanFormState,
+}) => {
+  useEffect(() => {
+    // When form mounts, clean its state
+    cleanFormState()
+    // eslint-disable-next-line
+  }, [])
   return (
-    <div className='px-20 py-12 w-136 rounded-lg shadow-lg border border-gray-300'>
-      <h2 className='text-4xl mb-12 font-thin text-center'>Create Your Account</h2>
-      <form className='text-xl' onSubmit={handleSubmit}>
+    <div className="px-20 py-12 w-136 rounded-lg shadow-lg border border-gray-300">
+      <h2 className="text-4xl mb-12 font-thin text-center">
+        Create Your Account
+      </h2>
+      <form className="text-xl" onSubmit={handleSubmit}>
         <Field
-          className='block rounded-full py-3 px-5 bg-white border border-gray-400 mx-auto w-full'
-          type='text'
-          name='name'
-          id='name'
-          placeholder='Name'
+          className="block rounded-full py-3 px-5 bg-white border border-gray-400 mx-auto w-full"
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Name"
         />
         <FormError message={errors.name} touched={touched.name} />
 
         <Field
-          className='block rounded-full py-3 px-5 bg-white border border-gray-400 mx-auto w-full'
-          type='email'
-          name='email'
-          id='email'
-          placeholder='e-mail'
+          className="block rounded-full py-3 px-5 bg-white border border-gray-400 mx-auto w-full"
+          type="email"
+          name="email"
+          id="email"
+          placeholder="e-mail"
         />
         <FormError message={errors.email} touched={touched.email} />
 
         <Field
-          className='block rounded-full py-3 px-5 bg-white border border-gray-400 mx-auto w-full'
-          type='password'
-          name='password'
-          id='password'
-          placeholder='Password'
+          className="block rounded-full py-3 px-5 bg-white border border-gray-400 mx-auto w-full"
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
         />
         <FormError message={errors.password} touched={touched.password} />
 
         <button
-          className='block rounded-full mt-10 py-3 px-5 bg-indigo-600 border border-indigo-600 mx-auto w-full text-white'
-          type='submit'
+          className="block rounded-full mt-10 py-3 px-5 bg-indigo-600 border border-indigo-600 mx-auto w-full text-white"
+          type="submit"
         >
           Sign Up
         </button>
-        {<p className='text-lg font-semibold'>{formState.message}</p>}
+        {<p className="text-lg font-semibold">{formState.message}</p>}
       </form>
     </div>
   )
@@ -61,17 +75,17 @@ const EnhancedRegisterForm = withFormik({
     return {
       name: name || '',
       email: email || '',
-      password: password || ''
+      password: password || '',
     }
   },
   validationSchema: validationSchema,
-  handleSubmit: registerUser
+  handleSubmit: registerUser,
 })(RegisterForm)
 
 function mapStatetoProps(store) {
   return {
     authState: store.authState,
-    formState: store.formState
+    formState: store.formState,
   }
 }
 
@@ -81,6 +95,7 @@ export default connect(
     onLogin,
     onLogout,
     onRegistrationCompleted,
-    onRegistrationFailed
+    onRegistrationFailed,
+    cleanFormState,
   }
 )(EnhancedRegisterForm)
