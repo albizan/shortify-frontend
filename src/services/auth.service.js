73970f0e8b5cf36dc78129bfdab683422a1f1a66
Alberto <albizan@hotmail.it>
2019-10-16 14:26:10 +0200

    Refactor auth service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,26 +1,27 @@
 import http from '../apis'
+import { history } from '../helpers'
 
 export async function registerUser(values, { props, setSubmitting, setErrors, resetForm }) {
+  // Disable submit button
   setSubmitting(true)
+
   try {
     const { data } = await http.post('/auth/register', {
       name: values.name,
       email: values.email,
-      password: values.password
+      password: values.password,
     })
-    const user = JSON.stringify(data.user)
-    const jwt = JSON.stringify(data.jwt)
-    localStorage.setItem('user', user)
-    localStorage.setItem('jwt', jwt)
-    resetForm()
 
-    // Set user in redux store
-    props.onLogin(data.user)
+    // Store user information
+    props.onLogin(data)
 
     // Set Form message
     props.onRegistrationCompleted('Registration complete, you can now log in')
+
+    // Clean all form fields
+    resetForm()
   } catch (error) {
-    // Remove user info from redux store
+    // Remove user information from redux store
     props.onLogout()
 
     // Check if error object has a response from server
@@ -28,37 +29,48 @@ export async function registerUser(values, { props, setSubmitting, setErrors, re
       const { status } = error.response
       if (status === 409) {
         setErrors({
-          name: 'Email already used'
+          name: 'Email already used',
         })
       }
     } else {
-      // else if we cannot get a response from the server, show an error message to the user
+      // if server doesn't respond, show an error message to the user
       props.onRegistrationFailed('Service unavailable')
     }
   } finally {
+    // Enable submit button
     setSubmitting(false)
   }
 }
 
 export async function loginUser(values, { props, setSubmitting, resetForm }) {
+  // Disable submit button
   setSubmitting(true)
+
   try {
+    // Send credentials
     const { data } = await http.post('/auth/login', {
       email: values.email,
-      password: values.password
+      password: values.password,
     })
-    const user = JSON.stringify(data.user)
-    const jwt = JSON.stringify(data.jwt)
-    localStorage.setItem('user', user)
-    localStorage.setItem('jwt', jwt)
-    resetForm()
-    // Set user in redux store
-    props.onLogin(data.user)
+
+    // Store user information
+    props.onLogin(data)
+
+    // Set form message in redux store
     props.onLoginCompleted('Success, you will be redirected to your dashboard')
+
+    // Clean all form fields
+    resetForm()
+
+    // Redirect user to his dashboard
+    setTimeout(() => {
+      history.push('/dashboard')
+    }, 1500)
   } catch (error) {
     props.onLogout()
     props.onLoginFailed('Invalid Credentials')
   } finally {
+    // Enable submit button
     setSubmitting(false)
   }
 }
