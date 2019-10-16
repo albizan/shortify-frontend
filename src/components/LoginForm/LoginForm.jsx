dbbb20f140c35dc04a6d74c7194d988790d6741f
Alberto <albizan@hotmail.it>
2019-10-16 22:28:51 +0200

    Remove input outline

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -4,6 +4,7 @@ import { Field, withFormik } from 'formik'
 import validationSchema from './validationSchema'
 
 import FormError from '../FormError'
+import { history } from '../../helpers'
 import { loginUser } from '../../services'
 import {
   onLogin,
@@ -13,15 +14,16 @@ import {
   cleanFormState,
 } from '../../redux/actions'
 
-const RegisterForm = ({
-  handleSubmit,
-  errors,
-  touched,
-  formState,
-  cleanFormState,
-}) => {
+const RegisterForm = ({ handleSubmit, errors, touched, authState, formState, cleanFormState }) => {
   useEffect(() => {
+    // Run code when component mounts
+    // Clean form fields
     cleanFormState()
+
+    // Check if user is already logged in, if so redirect to his dashboard
+    if (authState.isLoggedIn) {
+      history.push('/dashboard')
+    }
     // eslint-disable-next-line
   }, [])
   return (
@@ -29,7 +31,7 @@ const RegisterForm = ({
       <h2 className="text-4xl mb-12 font-thin text-center">Log In</h2>
       <form className="text-xl" onSubmit={handleSubmit}>
         <Field
-          className="block rounded-full py-3 px-5 bg-white border border-gray-400 mx-auto w-full"
+          className="block rounded-full outline-none focus:border-indigo-300 py-3 px-5 bg-white border border-gray-400 mx-auto w-full"
           type="email"
           name="email"
           id="email"
@@ -38,7 +40,7 @@ const RegisterForm = ({
         <FormError message={errors.email} touched={touched.email} />
 
         <Field
-          className="block rounded-full py-3 px-5 bg-white border border-gray-400 mx-auto w-full"
+          className="block rounded-full outline-none focus:border-indigo-300 py-3 px-5 bg-white border border-gray-400 mx-auto w-full"
           type="password"
           name="password"
           id="password"
@@ -47,7 +49,7 @@ const RegisterForm = ({
         <FormError message={errors.password} touched={touched.password} />
 
         <button
-          className="block rounded-full mt-10 py-3 px-5 bg-indigo-600 border border-indigo-600 mx-auto w-full text-white"
+          className="block rounded-full outline-none focus:outline-none mt-10 py-3 px-5 bg-indigo-600 hover:bg-indigo-700 border border-indigo-600 mx-auto w-full text-white"
           type="submit"
         >
           Log In
