328d1fe7a0b6b1900f3981396b9fff3db69c3df9
Alberto <albizan@hotmail.it>
2019-10-21 22:34:33 +0200

    Cleaned UI

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,26 +1,26 @@
 import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
-const Landing = props => {
+import Navbar from '../../components/Navbar'
+
+const Landing = (props) => {
   return (
     <Fragment>
-      <div className="w-full h-screen text-center">
-        <div className="h-half w-full bg-indigo-600 text-white flex items-center justify-center text-3xl">
-          Wecome To Shorter
-        </div>
-        <div className="h-half w-full flex flex-col items-center justify-center">
-          <Link
-            to="register"
-            className="cursor-pointer w-40 border border-indigo-600 rounded-full p-3 m-3"
-          >
-            Register
-          </Link>
-          <Link
-            to="login"
-            className="cursor-pointer w-40 border border-indigo-600 rounded-full p-3 m-3"
-          >
-            Log In
-          </Link>
+      <div className="w-full h-screen bg-indigo-600 relative">
+        <Navbar />
+        <div className="h-full w-full flex items-center justify-center">
+          <div className="text-center">
+            <h2 className="text-6xl text-gray-100 font-bold tracking-widest">Shortify</h2>
+            <h4 className="text-xl text-gray-100 font-semibold mb-10">
+              Shorten and manage your links with ease
+            </h4>
+            <Link
+              className="bg-gray-100 text-gray-800 rounded-full px-5 py-4 font-bold uppercase tracking-wide"
+              to="/register"
+            >
+              Register Now
+            </Link>
+          </div>
         </div>
       </div>
     </Fragment>
