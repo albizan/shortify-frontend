65b7b053c31868bd73526375775198da17246aff
Alberto <albizan@hotmail.it>
2019-10-22 15:00:27 +0200

    Redirect to login page if user is not logged in

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 
-const Dashboard = () => {
-  return <div>Dashboard</div>
+import Navbar from '../../components/Navbar'
+
+const Dashboard = (props) => {
+  const { authState } = props
+
+  // If there is no user logged in, redirect to login page
+  if (!authState.isLoggedIn) return <Redirect to="/login" />
+  return <Navbar />
 }
 
-export default Dashboard
+function mapStateToProps(store) {
+  return {
+    authState: store.authState,
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
