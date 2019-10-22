10cda5abbabbf83b80965ddd57da42d4ddbc9a57
Alberto <albizan@hotmail.it>
2019-10-22 17:35:03 +0200

    Give Navbar a position

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,6 +5,10 @@ import { Link } from 'react-router-dom'
 import Navbar from '../../components/Navbar'
 
 const LandingBackground = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  user-select: none;
   background: #8e2de2; /* fallback for old browsers */
   background: -webkit-linear-gradient(to left, #4a00e0, #8e2de2); /* Chrome 10-25, Safari 5.1-6 */
   background: linear-gradient(
@@ -17,16 +21,18 @@ const LandingBackground = styled.div`
 const Landing = (props) => {
   return (
     <Fragment>
-      <LandingBackground className="w-full h-screen relative">
-        <Navbar />
+      <LandingBackground>
+        <Navbar position="absolute" />
         <div className="h-full w-full flex items-center justify-center">
-          <div className="text-center">
-            <h2 className="text-6xl text-gray-100 font-bold tracking-widest">Shortify</h2>
-            <h4 className="text-xl text-gray-100 font-semibold mb-10">
+          <div className="text-center px-4 mt-20">
+            <h2 className="text-5xl sm:text-6xl text-gray-100 font-bold tracking-widest">
+              Shortify
+            </h2>
+            <h4 className="text-base sm:text-xl text-gray-100 mb-10">
               Shorten and manage your links with ease
             </h4>
             <Link
-              className="bg-gray-100 text-gray-800 rounded-full px-5 py-4 font-bold uppercase tracking-wide"
+              className="bg-gray-200 text-gray-800 rounded-full px-5 py-4 text-sm sm:text-base font-bold uppercase tracking-wide"
               to="/register"
             >
               Register Now
