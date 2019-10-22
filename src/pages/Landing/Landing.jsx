a0dd8fb1a1be4070ec152cba6907d5dda5c06582
Alberto <albizan@hotmail.it>
2019-10-22 14:59:27 +0200

    Added Gradient with styled components

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,12 +1,23 @@
 import React, { Fragment } from 'react'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 import Navbar from '../../components/Navbar'
 
+const LandingBackground = styled.div`
+  background: #8e2de2; /* fallback for old browsers */
+  background: -webkit-linear-gradient(to left, #4a00e0, #8e2de2); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to left,
+    #4a00e0,
+    #8e2de2
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+`
+
 const Landing = (props) => {
   return (
     <Fragment>
-      <div className="w-full h-screen bg-indigo-600 relative">
+      <LandingBackground className="w-full h-screen relative">
         <Navbar />
         <div className="h-full w-full flex items-center justify-center">
           <div className="text-center">
@@ -22,7 +33,7 @@ const Landing = (props) => {
             </Link>
           </div>
         </div>
-      </div>
+      </LandingBackground>
     </Fragment>
   )
 }
