255c7822e42ead7360fa80e4b6ba2909077343be
Alberto Zanotti <albizan@hotmail.it>
2019-11-07 20:50:51 +0100

    Show message if token is valid

--- src/pages/ConfirmEmail/ConfirmEmail.jsx
+++ src/pages/ConfirmEmail/ConfirmEmail.jsx
@@ -4,29 +4,37 @@ import Navbar from '../../components/Navbar'
 
 const ConfirmEmail = props => {
   const [loading, setLoading] = useState(false)
-  const [isConfirmed, setConfirmed] = useState(false)
+  const [isVerified, setVerified] = useState(false)
 
   // Function called when component mounts, send token to server
   useEffect(() => {
     async function sendToken() {
       try {
-        const { data } = await http.post('/confirm-email', {
-          token: props.match.params.token
-        })
-        console.log(data)
+        setLoading(true)
+        const { token } = props.match.params
+        const result = await http.get(`mail/confirm/${token}`)
+        if (result.status === 200) {
+          console.log('Received OK')
+          setVerified(true)
+        }
       } catch (error) {
         console.log(error)
+      } finally {
+        setLoading(false)
       }
     }
 
     sendToken()
-  })
+  }, [props.match.params.token])
 
   return (
     <Fragment>
       <Navbar position="absolute" theme="dark" />
       <section className="h-screen w-full flex items-center justify-center">
-        <div className=""></div>
+        {loading && <p className="text-xl">Please wait</p>}
+        {isVerified && (
+          <p className="text-xl">Account verified, you can now log in</p>
+        )}
       </section>
     </Fragment>
   )
