c81e63118e0d390c4af44890af282077e5930635
Alberto Zanotti <albizan@hotmail.it>
2019-11-19 14:06:15 +0100

    Validate jwt on mounting phase

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,21 +1,51 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 
-import Navbar from '../../components/Navbar'
+import { onLogout } from '../../redux/actions'
+import { history } from '../../helpers'
+import http from '../../apis'
 
-const Dashboard = (props) => {
-  const { authState } = props
+const Dashboard = ({ authState, onLogout }) => {
+  const [user, setUser] = useState(undefined)
 
-  // If there is no user logged in, redirect to login page
-  if (!authState.isLoggedIn) return <Redirect to="/login" />
-  return <Navbar />
+  // Check if local accessToken is still valid
+  useEffect(() => {
+    async function getUserInfo() {
+      try {
+        const response = await http.get('user/me')
+        setUser(response.data)
+      } catch (error) {
+        // @todo logout programmatically
+        onLogout()
+        history.push('/signin')
+      }
+    }
+    getUserInfo()
+  }, [])
+
+  // If there is no user logged in, redirect to sign in page
+  if (!authState.isLoggedIn) return <Redirect to="/signin" />
+
+  if (!user) {
+    return null
+  }
+
+  return (
+    <Fragment>
+      <div>
+        <p>Welcome Back {user ? user.name : 'Anonymous'}</p>
+      </div>
+    </Fragment>
+  )
 }
 
 function mapStateToProps(store) {
   return {
-    authState: store.authState,
+    authState: store.authState
   }
 }
 
-export default connect(mapStateToProps)(Dashboard)
+export default connect(mapStateToProps, {
+  onLogout
+})(Dashboard)
