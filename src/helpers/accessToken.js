ef36e372e5228f2f7ea09ee1b9c48c30c6cd4bb5
Alberto Zanotti <albizan@hotmail.it>
2019-11-19 14:05:47 +0100

    Return empty string if accessToken is not present

--- src/helpers/accessToken.js
+++ src/helpers/accessToken.js
@@ -2,7 +2,7 @@ export function saveAccessToken(accessToken) {
   localStorage.setItem('accessToken', accessToken)
 }
 export function retreiveAccessToken() {
-  return localStorage.getItem('accessToken')
+  return localStorage.getItem('accessToken') || ''
 }
 export function deleteAccessToken() {
   localStorage.removeItem('accessToken')
