6e2037fc773d5b1755e2263b98ba1dc5256ee444
Alberto Zanotti <albizan@hotmail.it>
2019-11-20 21:23:59 +0100

    Add setUser reducer

--- src/redux/reducers/auth.reducer.js
+++ src/redux/reducers/auth.reducer.js
@@ -1,9 +1,10 @@
-import { USER_LOGS_IN, USER_LOGS_OUT } from '../types'
+import { USER_LOGS_IN, USER_LOGS_OUT, SET_USER } from '../types'
 import { saveAccessToken, deleteAccessToken } from '../../helpers'
 
 const INITIAL_AUTH_STATE = {
   isLoggedIn: false,
-  accessToken: null
+  accessToken: null,
+  user: null
 }
 
 export function authReducer(authState = INITIAL_AUTH_STATE, action) {
@@ -21,7 +22,13 @@ export function authReducer(authState = INITIAL_AUTH_STATE, action) {
       return {
         ...authState,
         isLoggedIn: false,
-        accessToken: null
+        accessToken: null,
+        user: null
+      }
+    case SET_USER:
+      return {
+        ...authState,
+        user: payload
       }
     default:
       return authState
