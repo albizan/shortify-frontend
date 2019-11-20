37ce3b7f2d76327eff343e1bb50d376c642c0a55
Alberto Zanotti <albizan@hotmail.it>
2019-11-20 21:24:12 +0100

    Persist of redux store is removed

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,21 +1,7 @@
 import { createStore } from 'redux'
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
 import reducers from '../reducers'
 
-const persistConfig = {
-  key: 'root',
-  storage,
-}
-
-const persistedReducer = persistReducer(persistConfig, reducers)
-
 export const store = createStore(
-  persistedReducer,
-  // Used to activate redux dev tools extension for google chrome
+  reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
-
-export const persistor = persistStore(store)
-
-/* persistor is used by redux-persistor to persist redux store in localstorage */
