2b7f033c943f637cc1064b28216fb78bd94eb45c
Alberto Zanotti <albizan@hotmail.it>
2019-12-11 18:33:56 +0100

    Remove New Link page from router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,7 +9,6 @@ import Landing from '../../pages/Landing/Landing'
 import ConfirmEmail from '../../pages/ConfirmEmail'
 import PrivateRoute from '../PrivateRoute'
 import Dashboard from '../../pages/Dashboard'
-import NewLink from '../../pages/NewLink'
 
 const App = () => {
   return (
@@ -20,7 +19,6 @@ const App = () => {
           <Route path="/signup" component={RegisterPage} />
           <Route path="/signin" component={LoginPage} />
           <Route path="/confirm-email/:token" component={ConfirmEmail} />
-          <Route path="/new-link" component={NewLink} />
           <Route exact path="/not-found" component={Landing} />
           <PrivateRoute path="/dashboard" component={Dashboard} />
           <Redirect exact to="/not-found" />
