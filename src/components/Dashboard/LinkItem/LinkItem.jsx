085f9961067952161b1e13b618ab7c69a8cf6d80
Alberto Zanotti <albizan@hotmail.it>
2019-12-14 17:49:56 +0100

    Added buttons

--- src/components/Dashboard/LinkItem/LinkItem.jsx
+++ src/components/Dashboard/LinkItem/LinkItem.jsx
@@ -1,57 +1,102 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import { connect } from 'react-redux'
-import { removeLink } from '../../../redux/actions'
-import { deleteLink } from '../../../services'
+import styled from 'styled-components'
+import {
+  removeLink,
+  increaseActiveCount,
+  decreaseActiveCount
+} from '../../../redux/actions'
+import { deleteLink, toggleIsActive } from '../../../services'
 
-import { FaTrashAlt, FaEdit, FaLink } from 'react-icons/fa'
+import { FaArrowRight, FaTimes, FaCopy, FaEllipsisV } from 'react-icons/fa'
+
+const ToggleButton = styled.div`
+  top: -0.25rem;
+  left: ${({ isActive }) => {
+    if (isActive) return '1rem'
+    return '-0.25rem'
+  }};
+  transition: all 0.3s ease-in-out;
+  background-color: ${({ isActive }) => {
+    if (isActive) return '#2ecc71'
+    return '#e8f5e9'
+  }};
+`
 
 const LinkItem = props => {
-  const { link, removeLink } = props
+  const { link, removeLink, increaseActiveCount, decreaseActiveCount } = props
+  const { id, title, original, isActive } = link
+
+  const [active, setActive] = useState(isActive)
+  const [open, setOpen] = useState(false)
+
+  const handleToggle = () => {
+    if (active) {
+      decreaseActiveCount()
+    } else {
+      increaseActiveCount()
+    }
+    toggleIsActive(id, !active)
+    setActive(!active)
+  }
+
   return (
     <Fragment>
-      <div className="flex justify-between items-center border rounded my-6 p-6 shadow bg-white">
-        <div>
-          <div className="mb-3">
-            <p className="text-sm w-96 truncate">
-              <span className="font-semibold text-base">Original: </span>
-              {link.original}
-            </p>
+      <div className="my-4 rounded border-2 border-gray-200">
+        <div className="flex justify-between h-20">
+          <div className="flex justify-start items-center">
+            <div className="flex items-center justify-center mx-3">
+              <div className="relative">
+                <input id="toogle" type="checkbox" className="hidden" />
+
+                <div className="toggle__line w-10 h-4 bg-gray-300 rounded-full shadow-inner"></div>
+
+                <ToggleButton
+                  onClick={handleToggle}
+                  isActive={active}
+                  className="absolute w-6 h-6 rounded-full shadow inset-y-0 left-0 cursor-pointer"
+                ></ToggleButton>
+              </div>
+            </div>
+            <div>
+              <h3 className="text-xl">{title}</h3>
+              <h4 className="text-sm w-full truncate"> {original} </h4>
+            </div>
           </div>
-          <div className="mt-3">
-            <p className="text-sm w-96 truncate">
-              <span className="font-semibold text-base">Short: </span>
-              localhost:3000/{link.id}
-            </p>
-          </div>
-        </div>
-        <div className="flex justify-between w-56">
-          <p className="text-sm">
-            <span className="font-semibold text-base">Clicks: </span>
-            {link.clicks}
-          </p>
-          {link.isActive && (
-            <p className="text-sm">
-              <span className="font-semibold text-base">Active:</span> True
-            </p>
-          )}
-          {!link.isActive && (
-            <p className="text-sm">
-              <span className="font-semibold text-base">Active:</span> False
-            </p>
-          )}
-        </div>
-        <div className="flex justify-end">
-          <div
-            onClick={() => deleteLink(link.id, removeLink)}
-            className="w-12 h-12 bg-red-500 rounded-full mx-2 flex justify-center items-center cursor-pointer"
-          >
-            <FaTrashAlt className="text-white" />
-          </div>
-          <div className="w-12 h-12 bg-green-500 rounded-full mx-2 flex justify-center items-center cursor-pointer">
-            <FaEdit className="text-white" />
-          </div>
-          <div className="w-12 h-12 bg-blue-500 rounded-full mx-2 flex justify-center items-center cursor-pointer">
-            <FaLink className="text-white" />
+          <div className="flex items-center p-3">
+            {open && (
+              <div className="flex items-center justify-around">
+                <div className="mr-4">
+                  <div
+                    onClick={() => {
+                      setOpen(false)
+                    }}
+                    className="w-10 h-10 bg-gray-500 p-2 rounded-full flex justify-center items-center cursor-pointer"
+                  >
+                    <FaArrowRight className="text-xl text-gray-100" />
+                  </div>
+                </div>
+                <div className="flex justify-end items-center">
+                  <div
+                    onClick={() => deleteLink(id, removeLink)}
+                    className="w-10 h-10 bg-red-500 p-2 rounded-full flex justify-center items-center cursor-pointer mr-4"
+                  >
+                    <FaTimes className="text-xl text-gray-100" />
+                  </div>
+                  <div className="w-10 h-10 bg-indigo-500 p-2 rounded-full flex justify-center items-center cursor-pointer">
+                    <FaCopy className="text-xl text-gray-100" />
+                  </div>
+                </div>
+              </div>
+            )}
+            {!open && (
+              <FaEllipsisV
+                onClick={() => {
+                  setOpen(true)
+                }}
+                className="cursor-pointer text-xl text-gray-500 hover:text-gray-800"
+              />
+            )}
           </div>
         </div>
       </div>
@@ -59,4 +104,8 @@ const LinkItem = props => {
   )
 }
 
-export default connect(null, { removeLink })(LinkItem)
+export default connect(null, {
+  removeLink,
+  increaseActiveCount,
+  decreaseActiveCount
+})(LinkItem)
