5ed9ae86c43221643f48ba7ce7d73fdbed311703
Alberto Zanotti <albizan@hotmail.it>
2019-12-29 23:06:03 +0100

    Fix bug, replace is a function

--- src/pages/Link/Link.jsx
+++ src/pages/Link/Link.jsx
@@ -10,7 +10,7 @@ const Link = props => {
     async function retreiveLink() {
       try {
         const data = await getOriginalLink(linkId)
-        window.location.replace = data.original
+        window.location.replace(data.original)
         setLinkIsValid(true)
       } catch (error) {
         setLinkIsValid(false)
